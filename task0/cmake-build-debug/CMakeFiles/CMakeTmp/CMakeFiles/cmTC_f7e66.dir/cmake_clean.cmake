9c242e0857d6b9f9bd08cee021bf0766ffb18548
Yaroshevskii <yaroshevskii@g.nsu.ru>
2019-09-17 01:01:55 +0700

    Revert "test"
    This reverts commit d10d5f41b679e7748fb8ee9a62ad63c99c585a87.

---
+++ task0/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_f7e66.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_f7e66.dir/feature_tests.c.o"
+  "cmTC_f7e66.pdb"
+  "cmTC_f7e66.exe"
+  "libcmTC_f7e66.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_f7e66.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
