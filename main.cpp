46e45835b05c24b47537d0007a9e8e734026f28c
andy <a.yaroshevskii@g.nsu.ru>
2019-10-22 20:13:37 +0700

    smth change

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
 
 
+//cell
+//generation
+//universe
 
-int main()
+class mCell
 {
+    int value;
+
+public:
+    mCell()
+    {
+        value = 0;
+    }
+    mCell& operator++ (int)
+    {
+        value++;
+        return *this;
+    }
+
+    int getvalue() const
+    {
+        return value;
+    }
+
+};
+
+class mGeneration
+{
+    unordered_map<int, mCell> space;
+public:
+    mGeneration()
+    {
+
+    }
+};
+
+int main() {
+
+    mCell mc;
+
+    mc++;
+    mc++;
+    cout << " i = " << mc.getvalue() << endl;
+
 
     return 0;
 }
