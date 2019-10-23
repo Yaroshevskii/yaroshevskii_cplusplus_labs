eaf463cd8268a508d64191c42b44f31de5e8f3fb
andy <a.yaroshevskii@g.nsu.ru>
2019-10-23 20:31:23 +0700

    smth add
    subj

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include <map>
 #include <vector>
-#include <unordered_map>
+#include <sstream>
+#include <bits/stdc++.h>
+#include <regex>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 using namespace std;
 
+#define AliveSymbol ('*')
+#define DeadSymbol ('_')
 
 //cell
 //generation
@@ -35,10 +42,50 @@ public:
 class mGeneration
 {
     unordered_map<int, mCell> space;
-public:
-    mGeneration()
-    {
+    int n=0, m=0;
 
+    int convert_double_index_to_single(int a, int b)
+    {
+        return a*m + b;
+    }
+
+    void parseString(string item, int string_number)
+    {
+        for (unsigned int i=0; i < item.size(); i++)
+        {
+            if (item[i] == AliveSymbol)
+            {
+                cout << " item = " << item[i] << " line = " << string_number << " row = " << i << " alive" << endl;
+             //   int index = convert_double_index_to_single(string_number, i);
+            }
+            if (item[i] == DeadSymbol)
+            {
+                cout << " item = " << item[i] << " line = " << string_number << " row = " << i << " dead" << endl;
+            }
+        }
+    }
+public:
+    mGeneration(string FileName)
+    {
+        ifstream in(FileName);
+        string str = "";
+        int string_num = 0;
+        if (in.is_open())
+        {
+            while (getline(in, str))
+            {
+                if (string_num == -1)
+                {
+                    //string[] words = str.Split(new char[] { ' ' });
+                }else
+                {
+                    parseString(str, string_num);
+                }
+                string_num++;
+                cout << str << endl;
+            }
+            in.close();
+        }
     }
 };
 
@@ -50,6 +97,7 @@ int main() {
     mc++;
     cout << " i = " << mc.getvalue() << endl;
 
+    mGeneration mg("world.txt");
 
     return 0;
 }
