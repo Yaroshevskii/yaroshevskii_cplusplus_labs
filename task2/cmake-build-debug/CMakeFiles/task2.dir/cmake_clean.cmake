48a76c649caf3332756d264f0a33398d1ae7867e
andy <a.yaroshevskii@g.nsu.ru>
2019-11-26 12:14:24 +0700

    initial
    sbj

---
+++ task2/cmake-build-debug/CMakeFiles/task2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task2.dir/main.cpp.o"
+  "task2.pdb"
+  "task2.exe"
+  "libtask2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
